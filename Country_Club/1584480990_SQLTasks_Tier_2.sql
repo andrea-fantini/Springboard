f9d0762594eedf536a1f39c2a649868b1a60748e
andrea-fantini <afantini@outlook.com>
2020-06-10 16:16:43 -0700

    Q4: retrieve the details of facilities with ID 1 and 5.

--- Country_Club/1584480990_SQLTasks_Tier_2.sql
+++ Country_Club/1584480990_SQLTasks_Tier_2.sql
@@ -54,7 +54,10 @@ AND `membercost` < 0.2 * `monthlymaintenance`
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
-
+SELECT *
+FROM `Facilities`
+WHERE `facid`
+IN ( 1, 5 )
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
